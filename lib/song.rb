8f528236ef7a55e5f6cbedf95c3a784c61f2a5e2
samuelconlon <conlonsp@yahoo.com>
2023-01-22 17:24:59 -0500

    commit tree 4b6d502bce07cdfebd7f8208b62a96326fe629a4

---
+++ lib/song.rb
@@ -0,0 +1,108 @@
+require 'pry'
+
+class Song
+  attr_accessor :name, :artist, :genre
+
+  def initialize(name, artist, genre)
+    @name = name
+    @artist = artist
+    @genre = genre
+    @@count += 1
+    @@artists << @artist
+    @@genres << @genre
+  end
+
+  @@count = 0
+  @@artists = []
+  @@genres = []
+
+  def self.count
+    @@count
+  end
+
+  def self.artists
+    @@artists.uniq
+  end
+
+  def self.genres
+    @@genres.uniq
+  end
+
+  def self.genre_count
+    @@genres.tally
+  end
+
+  def self.artist_count
+    @@artists.tally
+  end
+
+
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# attr_accessor :name, :artist, :genre
+
+# @@count = 0
+# @@artists = []
+# @@genres = []
+
+
+# def initialize(name, artist, genre)
+#   @name = name
+#   @artist = artist
+#   @genre = genre
+#   @@count += 1
+#   @@artists << self.artist
+#   @@genres << self.genre
+# end
+
+# def self.count
+#   @@count
+# end
+
+# def self.artists
+#   @@artists.uniq
+# end
+
+# def self.genres
+#   @@genres.uniq
+# end
+
+# def self.genre_count
+#   @@genres.tally
+# end
+
+# def self.artist_count
+#   @@artists.tally
+# end
